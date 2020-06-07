57b6a6c02cc40c2f3c624dc2f8aa1d08d884c625
Liem Nguyen <liemnguyen0111@gmail.com>
2020-06-07 04:15:39 -0700

    Updated

--- connection/index.js
+++ connection/index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 // let sequelize
 
 
-     const sequelize = new Sequelize(process.env.JAWSDB_URL, {
+     const sequelize = new Sequelize("mysql://jppqn91gv0lrph8u:xleq3bqq4v4q9xbx@sq65ur5a5bj7flas.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ml281dqbclp0zm1n", {
       dialect:  'postgres',
       protocol: 'postgres',
       logging:  true //false
