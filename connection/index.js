46cd091b771ae946429a8b9b324d55e562c63b37
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-06-07 03:54:09 -0700

    initial

--- connection/index.js
+++ connection/index.js
@@ -1,5 +1,7 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize( process.env.JAWSDB_URL || process.env.LOCAL_URL )
+const sequelize = new Sequelize(
+  process.env.JAWSDB_URL || process.env.LOCAL_URL
+);
 
-module.exports = sequelize
+module.exports = sequelize;
