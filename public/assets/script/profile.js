4b1a8350dae9b15efa3c3e3cdb137a9b418ff8df
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-06-07 02:43:20 -0700

    profile.css - friendbox added and unFriend button small, profile.js - unFriend function was added, friendRoutes - updated.

--- public/assets/script/profile.js
+++ public/assets/script/profile.js
@@ -1,66 +1,61 @@
-const socket = io()
+const socket = io();
 
-socket.on('newUserSignUp', message =>
-{
-    console.log(message)
-})
-
+socket.on("newUserSignUp", (message) => {
+  console.log(message);
+});
 
 // const moment = moment()
-console.log('This is TIME')
-console.log(moment().format('l LT'))
-
+console.log("This is TIME");
+console.log(moment().format("l LT"));
 
 const logOut = () => {
-    console.log('hello')
-    var date = new Date();
-    
-    var utcDate = new Date(date.toUTCString());
-    utcDate.setHours(utcDate.getHours());
-    var usDate = new Date(utcDate);
-    document.cookie = `name=''; expires = ${usDate.toUTCString()}`; 
-    window.location.replace("/");
-    }
+  console.log("hello");
+  var date = new Date();
 
-function loggedInStatus () {
+  var utcDate = new Date(date.toUTCString());
+  utcDate.setHours(utcDate.getHours());
+  var usDate = new Date(utcDate);
+  document.cookie = `name=''; expires = ${usDate.toUTCString()}`;
+  window.location.replace("/");
+};
+
+function loggedInStatus() {
   if (!document.cookie.split("=")[1]) {
-      console.log("Nothing")
+    console.log("Nothing");
   } else {
-      console.log('Something')
+    console.log("Something");
     axios
       .get(`/api/users/info/${document.cookie.split("=")[1]}`)
       .then(({ data }) => {
-        
-          generateRecentPost()
-          console.log(data.UserPost)
-          userPost(data.UserPost,data.UserInfo.FirstName,data.UserInfo.LastName)
+        generateRecentPost();
+        console.log(data.UserPost);
+        userPost(
+          data.UserPost,
+          data.UserInfo.FirstName,
+          data.UserInfo.LastName
+        );
         document.getElementById("loggedIn").innerHTML = `
               Logged in as ${data.UserInfo.FirstName} ${data.UserInfo.LastName}
               <button id='logOut' onclick='logOut()'>Log Out</button>
               `;
-
       })
       .catch((err) => console.log(err));
   }
 }
 
-function userPost(userPost,firstName,lastName)
-{
-  console.log(userPost)
-    userPost.forEach(post => { 
-        // console.log("In userpost")
-        // console.log(post)
-        let comments = post.comments.reduce((allComments, comments) =>
-        {
-            return allComments +=  
-           ` <p>Name: <span>${comments.title}</span></p>`
-            
-            // console.log(comments)
-        }
-        ,'')
+function userPost(userPost, firstName, lastName) {
+  console.log(userPost);
+  userPost.forEach((post) => {
+    // console.log("In userpost")
+    // console.log(post)
+    let comments = post.comments.reduce((allComments, comments) => {
+      return (allComments += ` <p>Name: <span>${comments.title}</span></p>`);
 
-        $('.main').prepend(
-            `  
+      // console.log(comments)
+    }, "");
+
+    $(".main").prepend(
+      `  
             <div class="myPost">
             <h3 class ="username"> ${firstName} ${lastName} : <span class ="postbody">${post.body}</span></h3>
             
@@ -75,72 +70,70 @@ function userPost(userPost,firstName,lastName)
               <input type="text" placeholder="Add your comment" />
               <input type="submit" name="Send" value="Send" />
             </form>
-          </div>`)
-            })  
-            
-            var date = new Date();
-            console.log(date)
+          </div>`
+    );
+  });
+
+  var date = new Date();
+  console.log(date);
 }
 
-
-  // Tim recent friend post
-  function generateRecentPost()
-  {
-      console.log(`${document.cookie.split("=")[1]}`)
-        axios.get(`api/posts/friendrecentposts/${document.cookie.split("=")[1]}`)
-        .then( ({data}) =>
-            {
-                // console.log(data)
-                // var utcDate = new Date(item.createdAt.toUTCString());
-                // utcDate.setHours(utcDate.getHours());
-                // var usDate = new Date(utcDate);
-                data.forEach(item => {
-                    console.log(item.createdAt)
-                    $(".postlist").prepend(
-                        `  <div class="userpost" data-postid='${item.id}'>
+// Tim recent friend post
+function generateRecentPost() {
+  console.log(`${document.cookie.split("=")[1]}`);
+  axios
+    .get(`api/posts/friendrecentposts/${document.cookie.split("=")[1]}`)
+    .then(({ data }) => {
+      // console.log(data)
+      // var utcDate = new Date(item.createdAt.toUTCString());
+      // utcDate.setHours(utcDate.getHours());
+      // var usDate = new Date(utcDate);
+      data.forEach((item) => {
+        console.log(item.createdAt);
+        $(".postlist").prepend(
+          `  <div class="userpost" data-postid='${item.id}'>
                         <button>
                         <p>User:${item.user.firstName}<p>
-                        <p> ${item.body.slice(0,20)}... </p>
+                        <p> ${item.body.slice(0, 20)}... </p>
                         <p>At: ${item.createdAt.split(/[a-zA-Z.]/)[1]} <p>
                        </button>
                       </div>`
-                    )
-                });
-              
-            }
-        )
-        .catch(err => console.error(err))
-  }
-
-// Add new post
-document.getElementById('post').addEventListener('click', event =>
-{
-  event.preventDefault()
-
-  let formData = new FormData(document.getElementById('POSTIT'))
-  // formData.append('files', )
-  if(document.getElementById('file').value.length > 0 || document.getElementById('posttext').value !== '')
-  {
-  axios.post('api/posts/addpost', formData)
-  .then(({ data }) => userPost([data[0].data],data[1].firstName,data[1].lastName))
-  .catch(err => console.error(err))
-  
-  // console.log(formData)
-
-  document.getElementById('file').value = null
-  document.getElementById('posttext').value = ''
+        );
+      });
+    })
+    .catch((err) => console.error(err));
 }
 
-document.getElementById('posttext').focus()
+// Add new post
+document.getElementById("post").addEventListener("click", (event) => {
+  event.preventDefault();
 
-})
+  let formData = new FormData(document.getElementById("POSTIT"));
+  // formData.append('files', )
+  if (
+    document.getElementById("file").value.length > 0 ||
+    document.getElementById("posttext").value !== ""
+  ) {
+    axios
+      .post("api/posts/addpost", formData)
+      .then(({ data }) =>
+        userPost([data[0].data], data[1].firstName, data[1].lastName)
+      )
+      .catch((err) => console.error(err));
+
+    // console.log(formData)
+
+    document.getElementById("file").value = null;
+    document.getElementById("posttext").value = "";
+  }
+
+  document.getElementById("posttext").focus();
+});
 
 // Add friend on click
 
-
 // //show a list of other users who are not friends yet with me - hoyeon
 const renderFriendSuggestion = () => {
-
   if (!document.cookie.split("=")[1]) {
     console.log("Nothing");
   } else {
@@ -150,9 +143,8 @@ const renderFriendSuggestion = () => {
       .then(({ data }) => {
         document.getElementById("friendSuggest").innerHTML = "";
         for (let i = 0; i < data.length; i++) {
-          console.log(data[i]);
           let notfriendElem = document.createElement("div");
-          notfriendElem.innerHTML = `<button id='add'  onclick=addFriend('${data[i].uuid}') > + ${data[i].firstName} ${data[i].lastName}</button>`;
+          notfriendElem.innerHTML = `<button id='add' onclick=addFriend('${data[i].uuid}') > + ${data[i].firstName} ${data[i].lastName}</button>`;
           document.getElementById("friendSuggest").append(notfriendElem);
         }
       })
@@ -162,7 +154,7 @@ const renderFriendSuggestion = () => {
 };
 
 // let addFriend = document.querySelector('#myfriend')
-function addFriend( id ) {
+function addFriend(id) {
   if (event.target.id === "add") {
     axios
       .post("api/friend/addfriend", {
@@ -177,18 +169,35 @@ function addFriend( id ) {
   }
 }
 
+//delete a friend from myfriendList - hoyeon
+function unFriend(id) {
+  if (event.target.id === "unFriend") {
+    axios
+      .delete("api/friend/unfriend", {
+        data: {
+          userUuid: document.cookie.split("=")[1],
+          friendUuid: id,
+        },
+      })
+      .then(() => {
+        renderMyFriends();
+        renderFriendSuggestion();
+      })
+      .catch((err) => console.error(err));
+  }
+}
 
 //show all users who are currently friends with me- hoyeon
 const renderMyFriends = () => {
-  
-  axios.get(`/api/users/info/${document.cookie.split("=")[1]}`)
+  axios
+    .get(`/api/users/info/${document.cookie.split("=")[1]}`)
     .then(({ data }) => {
       let friendData = data.UserFriends;
       console.log(friendData);
       document.getElementById("friendList").innerHTML = "";
       for (let i = 0; i < friendData.length; i++) {
         let friendElem = document.createElement("div");
-        friendElem.innerHTML = `<button id='myfriend'> ${friendData[i].firstName} ${friendData[i].lastName}</button>`;
+        friendElem.innerHTML = `<div class="friendbox"><button class='myfriend'> ${friendData[i].firstName} ${friendData[i].lastName}</button><button id='unFriend' onclick=unFriend('${friendData[i].Id}')><span>🗑️</span></button></div>`;
         document.getElementById("friendList").append(friendElem);
       }
     })
@@ -280,7 +289,7 @@ const renderMyFriends = () => {
 //   }
 // };
 
-// // Generate recent post 
+// // Generate recent post
 //   function generateRecentPost()
 //   {
 //       console.log(`${document.cookie.split("=")[1]}`)
@@ -300,13 +309,13 @@ const renderMyFriends = () => {
 //                       </div>`
 //                     )
 //                 });
-              
+
 //             }
 //         )
 //         .catch(err => console.error(err))
 //   }
 
-//   // Submit new post 
+//   // Submit new post
 //   document.getElementById('post').addEventListener('click', event =>
 // {
 //   event.preventDefault()
@@ -317,28 +326,28 @@ const renderMyFriends = () => {
 //   .catch(err => console.error(err))
 // })
 
-// // User post 
+// // User post
 // function userPost(userPost,firstName,lastName)
 // {
 //   console.log(userPost)
-//     userPost.forEach(post => { 
+//     userPost.forEach(post => {
 //         // console.log("In userpost")
 //         // console.log(post)
 //         let comments = post.comments.reduce((allComments, comments) =>
 //         {
-//             return allComments +=  
+//             return allComments +=
 //            ` <p>Name: <span>${comments.title}</span></p>`
-            
+
 //             // console.log(comments)
 //         }
 //         ,'')
 
 //         $('.main').prepend(
-//             `  
+//             `
 //             <div class="myPost">
 //             <p>${firstName} ${lastName}</p>
 //             <p>${post.body}</p>
-//             <div class="image"> 
+//             <div class="image">
 //             <img src="${post.image}" alt="" class="postImage">
 //                </div>
 //             <hr />
@@ -350,9 +359,8 @@ const renderMyFriends = () => {
 //               <input type="submit" name="Send" value="Send" />
 //             </form>
 //           </div>`)
-//             })            
+//             })
 // }
-
 
 function init() {
   loggedInStatus();
